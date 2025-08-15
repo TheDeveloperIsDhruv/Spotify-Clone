9425e60c6c610c8d438350de6d7d65dc8dad45e5
TheDeveloperIsDhruv <108569661+TheDeveloperIsDhruv@users.noreply.github.com>
2025-08-15 13:31:38 -0400

    Added the backend work for signup page

--- Backend/server.js
+++ Backend/server.js
@@ -13,7 +13,6 @@ app.get("/", (req, res) => {
 
 app.post('/api/submit-form',(req,res)=>{
    const {email,password}=req.body;
-
    if(!email || !password){
         return res.status(400).json({message:"Email and Password are required fields!"})
    }
@@ -21,6 +20,18 @@ app.post('/api/submit-form',(req,res)=>{
    console.log("Received from data:",{email,password});
    return res.json({message:"successfully!"});
    }
+
+});
+
+app.post('/api/submit-signup',(req,res)=>{
+    const {firstname,lastname,dob,email,password}=req.body;
+    if(!firstname || !lastname || !dob || !email || !password){
+      return res.status(400).json({message:"Email and Password are required fields!"})
+    }
+    else{
+    console.log("Received from data:",{firstname,lastname,dob,email,password});
+    return res.json({message:"successfully!"});
+    }
 
 });
 
