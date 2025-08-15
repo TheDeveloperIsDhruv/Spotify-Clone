14e3aecbaf80d5118896c41979226e730f3f31c1
TheDeveloperIsDhruv <108569661+TheDeveloperIsDhruv@users.noreply.github.com>
2025-08-15 01:27:17 -0400

    Added backend work for login page

---
+++ Backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import cors from 'cors';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Middleware to parse JSON request bodies
+
+app.get("/", (req, res) => {
+  res.send("Hello, Express with ES Modules!");
+});
+
+app.post('/api/submit-form',(req,res)=>{
+   const {email,password}=req.body;
+
+   if(!email || !password){
+        return res.status(400).json({message:"Email and Password are required fields!"})
+   }
+   else{
+   console.log("Received from data:",{email,password});
+   return res.json({message:"successfully!"});
+   }
+
+});
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
